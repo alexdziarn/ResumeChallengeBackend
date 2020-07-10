bb7afc4b6f9e91f5027e132f59e26fe4a5b29006
alex-windows-pc <alexdziarn@gmail.com>
2020-07-10 19:13:04 -0400

    change if statement syntax

--- view-count/test_main.py
+++ view-count/test_main.py
@@ -4,7 +4,7 @@ import json
 
 def test_lambda_handler():
     test_results = json.dumps(lambda_handler())
-    if test_results is null
+    if not bool(test_results)
         a=1
         assert a==1
     else:
