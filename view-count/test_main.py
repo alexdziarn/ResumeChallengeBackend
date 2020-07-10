5b047399330355ffc65745fd65f01b7c592531fe
alex-windows-pc <alexdziarn@gmail.com>
2020-07-10 18:19:36 -0400

    added colon to else statement

--- view-count/test_main.py
+++ view-count/test_main.py
@@ -6,6 +6,6 @@ def test_lambda_handler():
     if "Item"not in boto3.resource('dynamodb').Table('MyWebsiteTable').get_item(Key={'type': 'resume'}):
         a=1
         assert a==1
-    else
+    else:
         test_results = json.dumps(lambda_handler())
         assert list(test_results.values())[1].values()[1] == boto3.resource('dynamodb').Table('MyWebsiteTable')['Item']['views']
