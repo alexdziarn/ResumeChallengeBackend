15814a8ba504824fe47e77fd17c5eeba1feac1b2
alex-windows-pc <alexdziarn@gmail.com>
2020-07-10 18:24:53 -0400

    added region

--- view-count/test_main.py
+++ view-count/test_main.py
@@ -3,9 +3,9 @@ import boto3
 import json
 
 def test_lambda_handler():
-    if "Item"not in boto3.resource('dynamodb').Table('MyWebsiteTable').get_item(Key={'type': 'resume'}):
+    if "Item"not in boto3.resource('dynamodb', region_name='us-east-1').Table('MyWebsiteTable').get_item(Key={'type': 'resume'}):
         a=1
         assert a==1
     else:
         test_results = json.dumps(lambda_handler())
-        assert list(test_results.values())[1].values()[1] == boto3.resource('dynamodb').Table('MyWebsiteTable')['Item']['views']
+        assert list(test_results.values())[1].values()[1] == boto3.resource('dynamodb', region_name='us-east-1').Table('MyWebsiteTable')['Item']['views']
