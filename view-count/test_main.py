23ac9b835e3dfcbccfcb33dd341e70f9b6858b90
alex-windows-pc <alexdziarn@gmail.com>
2020-07-10 19:49:36 -0400

    should pass since i fixed sam

--- view-count/test_main.py
+++ view-count/test_main.py
@@ -5,7 +5,7 @@ import json
 def test_lambda_handler():
     test_results = json.dumps(lambda_handler())
     if test_results==0:
-        a=1
-        assert a==1
+        a=2
+        assert a==2
     else:
         assert list(test_results.values())[1].values()[1] > 0
