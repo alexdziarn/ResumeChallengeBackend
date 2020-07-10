09c6591193d5770a89120ef25e78d48ee88c3ec1
alex-windows-pc <alexdziarn@gmail.com>
2020-07-10 18:16:31 -0400

    first official test

--- view-count/test_main.py
+++ view-count/test_main.py
@@ -1,6 +1,11 @@
 from main import lambda_handler
 import boto3
+import json
 
 def test_lambda_handler():
-    result = 4
-    assert result == 4
+    if "Item"not in boto3.resource('dynamodb').Table('MyWebsiteTable')get_item(Key={'type': 'resume'}):
+        a=1
+        assert a==1
+    else
+        test_results = json.dumps(lambda_handler())
+        assert list(test_results.values())[1].values()[1] == boto3.resource('dynamodb').Table('MyWebsiteTable')['Item']['views']
