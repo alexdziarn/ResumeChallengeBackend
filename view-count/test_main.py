bb71366b7b326ab43bc22f94acef373a413d12bc
alex-windows-pc <alexdziarn@gmail.com>
2020-07-10 20:21:01 -0400

    test fix for lambda_handler function

--- view-count/test_main.py
+++ view-count/test_main.py
@@ -5,7 +5,7 @@ import json
 from boto3.dynamodb.conditions import Key
 
 def test_lambda_handler():
-    test_results = json.dumps(lambda_handler({},{})
+    test_results = json.dumps(lambda_handler({'key1': 'value1', 'key2': 'value2', 'key3': 'value3'},{})
     client = boto3.resource('dynamodb')
     table = client.Table('MyWebsiteTable')
     response = table.get_item(Key={'type': 'resume'})
