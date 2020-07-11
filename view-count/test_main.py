2f51493f139cb63409c4d034fb4171b0402152c7
alex-windows-pc <alexdziarn@gmail.com>
2020-07-10 20:05:51 -0400

    change work flow and test function

--- view-count/test_main.py
+++ view-count/test_main.py
@@ -2,10 +2,11 @@ from main import lambda_handler
 import boto3
 import json
 
+from boto3.dynamodb.conditions import Key
+
 def test_lambda_handler():
-    test_results = json.dumps(lambda_handler())
-    if test_results==0:
-        a=2
-        assert a==2
-    else:
-        assert list(test_results.values())[1].values()[1] > 0
+    test_results = json.dumps(lambda_handler({},{})
+    client = boto3.resource('dynamodb')
+    table = client.Table('MyWebsiteTable')
+    response = table.get_item(Key={'type': 'resume'})
+    assert int(response['Item']['views']==json.loads(test_results)['body']['views']
